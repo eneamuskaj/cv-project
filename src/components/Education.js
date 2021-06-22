20635db60db17ed25983423bb5557e2ffaff3f14
Enea Muskaj <eneamuskaj@gmail.com>
2021-06-21 22:00:00 -0500

    added state to the education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,23 +1,63 @@
-import React from "react";
+import React from 'react'
 
 class Education extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      university: '',
+      major: '',
+      year: '',
+      gpa: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const { name, type } = event.target
+    this.setState({ [name]: event.target.value })
+    console.log(this.state)
+  }
   render() {
     return (
       <div className="container">
         <h1>Education</h1>
         <form>
-          <input type="text" name="name" placeholder="University" />
+          <input
+            type="text"
+            name="university"
+            placeholder="University"
+            value={this.state.university}
+            onChange={this.handleChange}
+          />
           <br />
-          <input type="text" name="name" placeholder="Major" />
+          <input
+            type="text"
+            name="major"
+            placeholder="Major"
+            value={this.state.major}
+            onChange={this.handleChange}
+          />
           <br />
-          <input type="text" name="name" placeholder="Year" />
+          <input
+            type="text"
+            name="year"
+            placeholder="Year"
+            value={this.state.year}
+            onChange={this.handleChange}
+          />
           <br />
-          <input type="text" name="name" placeholder="GPA" />
+          <input
+            type="text"
+            name="gpa"
+            placeholder="GPA"
+            value={this.state.gpa}
+            onChange={this.handleChange}
+          />
           <input type="submit" value="Submit" />
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default Education;
+export default Education
