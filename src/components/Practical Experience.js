1ccd20c487c63baff933da0f5b8fb572337d755f
eneamuskaj <eneamuskaj@gmail.com>
2021-06-22 17:44:39 -0500

    added state to practical experience

--- src/components/Practical Experience.js
+++ src/components/Practical Experience.js
@@ -1,16 +1,50 @@
 import React from "react";
 
 class PracticalExperience extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      company: "",
+      startingDate: "",
+      position: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    const { name, type } = event.target;
+    this.setState({
+      [name]: event.target.value,
+    });
+    console.log(this.state);
+  }
   render() {
     return (
       <div className="container">
         <h1>Practical Experience</h1>
         <form>
-          <input type="text" name="name" placeholder="Company" />
+          <input
+            type="text"
+            name="company"
+            placeholder="Company"
+            onChange={this.handleChange}
+            value={this.state.company}
+          />
           <br />
-          <input type="text" name="name" placeholder="Starting Date" />
+          <input
+            type="text"
+            name="startingDate"
+            placeholder="Starting Date"
+            onChange={this.handleChange}
+            value={this.state.startingDate}
+          />
           <br />
-          <input type="text" name="name" placeholder="Position" />
+          <input
+            type="text"
+            name="position"
+            placeholder="Position"
+            onChange={this.handleChange}
+            value={this.state.position}
+          />
           <br />
           <input type="submit" value="Submit" />
         </form>
