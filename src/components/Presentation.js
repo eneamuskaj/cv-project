cf58b1a3465630fe9060ee58d794c0beaa66b5e7
eneamuskaj <eneamuskaj@gmail.com>
2021-06-23 20:44:59 -0500

    added the print functionality

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,21 +1,31 @@
 import React from "react";
 
 class Presentation extends React.Component {
+  printContent() {
+    var printcontent = document.getElementById("div1").innerHTML;
+    document.body.innerHTML = printcontent;
+    window.print();
+  }
   render(props) {
     return (
       <div>
-        <h1>
-          {this.props.name} {this.props.lastName}
-        </h1>
-        <p>E-mail: {this.props.eMail}</p>
-        <p>Phone: {this.props.phone}</p>
-        <h3>University: {this.props.university}</h3>
-        <p>Major: {this.props.major}</p>
-        <p>Year of Graduation: {this.props.year}</p>
-        <p>GPA: {this.props.gpa}</p>
-        <h3>Current employer: {this.props.company}</h3>
-        <h4>Starting date: {this.props.startingDate}</h4>
-        <p>Position: {this.props.position}</p>
+        <div id="div1">
+          <h1>
+            {this.props.name} {this.props.lastName}
+          </h1>
+          <p>E-mail: {this.props.eMail}</p>
+          <p>Phone: {this.props.phone}</p>
+          <h3>University: {this.props.university}</h3>
+          <p>Major: {this.props.major}</p>
+          <p>Year of Graduation: {this.props.year}</p>
+          <p>GPA: {this.props.gpa}</p>
+          <h3>Current employer: {this.props.company}</h3>
+          <h4>Starting date: {this.props.startingDate}</h4>
+          <p>Position: {this.props.position}</p>
+        </div>
+        <div>
+          <button onClick={this.printContent}>Print Content</button>
+        </div>
       </div>
     );
   }
